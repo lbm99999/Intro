ca6dc3bf6f4861590793d4a85ff9e83d1ef573b3
lbm99999 <lbm0813@gmail.com>
2017-05-20 03:35:12 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_settings.xml

--- app/src/main/java/kr/p_e/mprof/www/intro/BtnMenu.java
+++ app/src/main/java/kr/p_e/mprof/www/intro/BtnMenu.java
@@ -2,15 +2,18 @@ package kr.p_e.mprof.www.intro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
 
-public class LoginActivity extends AppCompatActivity {
+public class BtnMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_btn_menu);
+
     }
 }
